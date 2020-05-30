c1ef80ba1d80f642df717449c454e541dc08f8ff
Yosep Parlindungan Naibaho <54306892+Yosep-del@users.noreply.github.com>
2020-05-30 23:36:27 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,11 +3,5 @@
  * using mysqli_connect for database connection
  */
 
-$databaseHost = 'localhost';
-$databaseName = 'crud_db';
-$databaseUsername = 'root';
-$databasePassword = '';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-
+$link = new mysqli($_SERVER['db-yosep.cswtsj9rjmlo.us-east-1.rds.amazonaws.com'], $_SERVER['admin'], $_SERVER['plplplpl090'], $_SERVER['db-yosep'], $_SERVER['3306']);
 ?>
