25b1da6fa8b1249ded7026d5fbcc338e4e5f389e
Yosep Parlindungan Naibaho <54306892+Yosep-del@users.noreply.github.com>
2020-05-31 15:20:31 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	$server = 'db-yosep.cswtsj9rjmlo.us-east-1.rds.amazonaws.com'';
 	$user = 'admin';
 	$pass = 'plplplpl090';
-	$db = 'tugas4';
+	$db = 'tugasadsis';
 
 	$con = mysqli_connect($server,$user,$pass,$db) or die("failed to connect");
 ?>
